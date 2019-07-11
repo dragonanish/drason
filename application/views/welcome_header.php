55b5959e6e0325809a21db540e9ce9457d3a8b6d
Anish stha <samit.puri@amniltech.com>
2019-07-11 20:47:09 +0545

    First commit of project drason

---
+++ application/views/welcome_header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Clean UI Website Template + One & Multi Page + Multipurpose One + Mobile Ready</title>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,900' rel='stylesheet' type='text/css'>
+
+    <link href="assets/welcome/common/img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
+    <link href="assets/welcome/common/img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
+    <link href="assets/welcome/common/img/favicon.72x72.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
+    <link href="assets/welcome/common/img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
+    <link href="assets/welcome/common/img/favicon.png" rel="icon" type="image/png">
+    <link href="favicon.ico" rel="shortcut icon">
+
+    <!-- HTML5 shim and Respond.js for < IE9 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9] -->
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <!--[endif]-->
+
+    <!-- Vendors Styles -->
+    <!-- v1.0.0 -->
+    <link rel="stylesheet" type="text/css" href="assets/welcome/vendors/bootstrap/dist/css/bootstrap.min.css">
+
+    <!-- Clean UI Admin Template Styles -->
+    <link rel="stylesheet" type="text/css" href="assets/welcome/common/css/source/main.css">
+
+    <!-- Vendors Scripts -->
+    <!-- v1.0.0 -->
+    <script src="assets/welcome/vendors/jquery/dist/jquery.min.js"></script>
+    <script src="assets/welcome/vendors/tether/dist/js/tether.min.js"></script>
+    <script src="assets/welcome/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/welcome/vendors/typed.js/dist/typed.min.js"></script>
+    <script src="assets/welcome/vendors/three/build/three.min.js"></script>
+    <script src="assets/welcome/vendors/jquery.scrollTo/jquery.scrollTo.min.js"></script>
+
+    <!-- Clean UI Admin Template Scripts -->
+    <script src="assets/welcome/common/js/common.js"></script>
+</head>
+<body>
+
+    <!-- Header Block Full -->
+    <div style="background-color: black" class="cwt__block cwt__header-fixed">
+        <div class="container">
+            <!-- <div class="cwt__header-fixed__container"> -->
+                <div  class="row">
+                    <div class="col-xs-1">
+                        <div class="cwt__logo cwt__logo--small">
+                            <a href="index.html">
+                                <img style="width: 80px" src="assets/welcome/common/img/logo.jpg" >
+                           </a> 
+                        </div>
+                    </div>
+                    <div  class="col-xs-10"><br>
+                        <nav class="nav nav-inline pull-xs-right cwt__main-menu cwt__main-menu--dark">
+                            <div class="cwt__mobile-menu">
+                                <a href="javascript: void(0);"  class="nav-link btn btn-success cwt__main-menu__link cwt__main-menu__link--button">Home</a>
+                                <a href="javascript: void(0);"  class="nav-link btn btn-success cwt__main-menu__link cwt__main-menu__link--button">OldProducts</a>
+                                <a href="javascript: void(0);"  class="nav-link btn btn-success cwt__main-menu__link cwt__main-menu__link--button">NewProducts</a>
+                                <a href="javascript: void(0);"  class="nav-link btn btn-success cwt__main-menu__link cwt__main-menu__link--button">DonationProducts</a>
+                                 <a href="javascript: void(0);"  class="nav-link btn btn-success cwt__main-menu__link cwt__main-menu__link--button">RecycleProducts</a>
+                                 <a href="javascript: void(0);" style="border-radius: 90px" class="nav-link btn btn-primary cwt__main-menu__link cwt__main-menu__link--button">Sign IN</a>
+                                  <a href="javascript: void(0);" style="border-radius: 90px" class="nav-link btn btn-primary cwt__main-menu__link cwt__main-menu__link--button">Sign Up</a>
+                            </div>
+                            
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- </div> -->
